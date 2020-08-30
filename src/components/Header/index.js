ddaccdcc81c2322ecc813d469c6e07ba17b790f5
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-08-29 22:59:24 -0300

    Implementação parcial do header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import logo from "../../logo.png";
 import "./styles.css";
 
-const Header = () => <h1>Hello World</h1>;
+const Header = () => (
+  <header className="main-header">
+    <img src={logo} alt="React Movies" />
+    <div className="main-menu">
+      <input type="text" placeholder="Pesquisar" />
+      <ul>
+        <li>Início</li>
+        <li>Filmes</li>
+        <li>Séries</li>
+      </ul>
+    </div>
+  </header>
+);
 
 export default Header;
