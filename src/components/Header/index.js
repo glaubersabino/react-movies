a2f53859e908aab981a44f1f091ea8917b4561ca
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-08-30 23:44:46 -0300

    Conclusão da estilização da página inicial e instalação e configuração do axios.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import logo from "../../logo.png";
+import logo from "../../assets/logo.png";
 import "./styles.css";
 
 const Header = () => (
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,99 +1,181 @@
-import React from "react";
+import React, { Component } from "react";
+import api from "../../services/api";
 
 import "./styles.css";
 import Header from "../../components/Header";
 
-const Main = () => (
-  <div className="main">
-    <Header />
+export default class Main extends Component {
+  state = {
+    popular: [],
+  };
 
-    <div className="posters">
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/dzOxNbbz1liFzHU1IPvdgUR647b.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/2yQUnpc1BXgesJrfcdoRa6jTAnA.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/f496cm9enuEsZkSPzCwnTESEK5s.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/b34jPzmB0wZy7EjUZoleXOl2RRI.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/vQiryp6LioFxQThywxbC6TuoDjy.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/wuMc08IPKEatf9rnMNXvIDxqP4W.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/g6n8G0xPnBVUIKKK41sNmbZB5h4.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/iGoXIpQb7Pot00EEdwpwPajheZ5.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/da22ZBmrDOXOCDRvr8Gic8ldhv4.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/v0wMKEEGaNc9evdqGYfIvoWXh24.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/Ag21otqbeynZPGjEbzyr3X5Us0j.jpg"
-          alt="The Boys"
-        />
-      </div>
-      <div className="poster">
-        <img
-          src="https://image.tmdb.org/t/p/w200/sdEOH0992YZ0QSxgXNIGLq1ToUi.jpg"
-          alt="The Boys"
-        />
-      </div>
-    </div>
-  </div>
-);
+  componentDidMount() {
+    this.loadMovies();
+    this.loadTVShows();
+  }
 
-export default Main;
+  loadMovies = async () => {
+    const response = await api.get(
+      "/movie/popular?api_key=e8ebd3655dbc3593ecab47c3565d5d9d&language=pt-BR&page=1"
+    );
+    console.log(response.data);
+  };
+
+  loadTVShows = async () => {
+    const response = await api.get(
+      "/tv/popular?api_key=e8ebd3655dbc3593ecab47c3565d5d9d&language=pt-BR&page=1"
+    );
+    console.log(response.data);
+  };
+
+  render() {
+    return (
+      <div className="main">
+        <div className="poster-bg">
+          <Header />
+          <div className="poster-featured-info">
+            <h1>Harry Potter e a Pedra Filosofal</h1>
+            <div className="info">
+              <p>98%</p>
+              <p>30/08/2020</p>
+              <span>13+</span>
+            </div>
+            <div className="actions">
+              <button className="btn btn-danger">Assistir</button>
+              <button className="btn btn-opacity">Favoritos</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="posters">
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/dzOxNbbz1liFzHU1IPvdgUR647b.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/2yQUnpc1BXgesJrfcdoRa6jTAnA.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/f496cm9enuEsZkSPzCwnTESEK5s.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/b34jPzmB0wZy7EjUZoleXOl2RRI.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/vQiryp6LioFxQThywxbC6TuoDjy.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/wuMc08IPKEatf9rnMNXvIDxqP4W.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/g6n8G0xPnBVUIKKK41sNmbZB5h4.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/iGoXIpQb7Pot00EEdwpwPajheZ5.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/da22ZBmrDOXOCDRvr8Gic8ldhv4.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/v0wMKEEGaNc9evdqGYfIvoWXh24.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/Ag21otqbeynZPGjEbzyr3X5Us0j.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/sdEOH0992YZ0QSxgXNIGLq1ToUi.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/7VRkmVB23rloRbZHsCRPscXlgQp.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/devZdyPRXMiN8HHiX7Y8s0LLEee.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/tX0o4AdHpidgniTWwfzK0dNTKrc.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/g6n7TdQSgozArIM0okXTjjCM9Np.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/89QTZmn34iwXYeCpVxhC9UrT8sX.jpg"
+              alt="The Boys"
+            />
+          </div>
+          <div className="poster">
+            <img
+              src="https://image.tmdb.org/t/p/w200/AkrRD3Lnwb82Stu2SpDKAerK6O2.jpg"
+              alt="The Boys"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
