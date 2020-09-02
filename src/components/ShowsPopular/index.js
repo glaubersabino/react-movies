4df2f1a1b2c4d642cc9bb66d6a53669ccb57de4a
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-09-02 12:55:39 -0300

    Conclusão do componente de séries.

--- src/components/ShowsPopular/index.js
+++ src/components/ShowsPopular/index.js
@@ -1,27 +1,5 @@
 import React, { Component } from "react";
-import { motion } from "framer-motion";
 import api from "../../services/api";
-
-const container = {
-  hidden: { opacity: 1, scale: 0 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delay: 0.3,
-      when: "beforeChildren",
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const item = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-};
 
 export default class ShowsPopular extends Component {
   state = {
@@ -36,29 +14,26 @@ export default class ShowsPopular extends Component {
     const response = await api.get(
       `/tv/popular?api_key=${process.env.REACT_APP_SECRET_API}&language=pt-BR&page=1`
     );
-    console.log(response.data);
+    this.setState({ shows: response.data.results });
   };
 
   render() {
+    const { shows } = this.state;
+
     return (
       <div className="main-posters">
         <h2>Séries em destaque</h2>
 
-        <motion.div
-          className="posters"
-          variants={container}
-          initial="hidden"
-          animate="visible"
-        >
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
-            <motion.div key={index} className="poster" variants={item}>
+        <div className="posters">
+          {shows.slice(0, 12).map((show) => (
+            <div key={show.id} className="poster">
               <img
-                src="https://image.tmdb.org/t/p/w200/dzOxNbbz1liFzHU1IPvdgUR647b.jpg"
-                alt="The Boys"
+                src={`https://image.tmdb.org/t/p/w200${show.poster_path}`}
+                alt={show.title}
               />
-            </motion.div>
+            </div>
           ))}
-        </motion.div>
+        </div>
       </div>
     );
   }
