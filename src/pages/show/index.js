895e92aa2815d19e66cc38895efde88ed4265f7b
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-09-08 13:47:43 -0300

    Implementação do componente Cast na single das séries.

--- src/pages/show/index.js
+++ src/pages/show/index.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 import Banner from "../../components/Banner";
+import Cast from "../../components/Cast";
 
 export default class Show extends Component {
   state = {
     show: [],
     genres: [],
+    cast: [],
   };
 
   componentDidMount() {
     this.loadShow();
+    this.loadCast();
   }
 
   loadShow = async () => {
@@ -21,11 +24,21 @@ export default class Show extends Component {
     this.setState({ show: response.data, genres: response.data.genres });
   };
 
+  loadCast = async () => {
+    const { id } = this.props.match.params;
+    const response = await api.get(
+      `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${process.env.REACT_APP_SECRET_API}`
+    );
+
+    this.setState({ cast: response.data.cast });
+  };
+
   render() {
-    const { show, genres } = this.state;
+    const { show, genres, cast } = this.state;
     return (
       <div className="main">
         <Banner data={show} gen={genres} />
+        <Cast cast={cast} />
       </div>
     );
   }
